028d959bf3e814e901fccc5c08dba27da8ccb139
Ricardo Santos <www.meumail@gmail.com>
2016-03-15 17:39:53 +0000

    Misc - Removed SQLite3 suport

--- GoodToGo_Swift/Bridging-Header.h
+++ GoodToGo_Swift/Bridging-Header.h
@@ -2,11 +2,6 @@
 //  Bridging-Header.h
 //
 
-// TODO : Remove sqlite3 suport
-// Frameworks.OpenSource/SQLite
-#import "sqlite3.h"
-#import <time.h>
-
 // Frameworks.OpenSource/SwiftyDropbox-master
 #import "SwiftyDropbox.h"
 #import "SecurityUtil.h"
